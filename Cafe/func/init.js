cf9f5679109f8364bb11d1c0b760dbf71c997fba
joshua <iuttn123@gmail.com>
2025-03-31 17:47:30 +0900

    현배

--- Cafe/func/init.js
+++ Cafe/func/init.js
@@ -4,25 +4,23 @@
 const fs = require("fs");
 const { env } = require("../env.js");
 const { Coffee } = require("./coffee.js");
+const { managers } = require("../manager/base.js");
 
 const initializeData = () => {
-  if (fs.existsSync(env.dataStore)) {
-    const data = fs.readFileSync(env.dataStore, "utf-8");
-    const obj = JSON.parse(data);
-    const { coffeeMenu } = obj;
+  const { data } = managers;
+  if (data.existData()) {
+    const getData = data.getData();
+    const { coffeeMenu } = getData;
     const newData = coffeeMenu.map((v) => new Coffee(v.name, v.stock, v.price));
     return newData;
   } else {
-    const initData = {
-      coffeeMenu: [
-        { name: "아메리카노", stock: 0, price: 3000 },
-        { name: "우유라떼", stock: 0, price: 3500 },
-        { name: "두유라떼", stock: 0, price: 4000 },
-      ],
-    };
-    fs.writeFileSync(env.dataStore, JSON.stringify(initData), "utf-8");
-    const data = fs.readFileSync(env.dataStore, "utf-8");
-    return JSON.parse(data);
+    const initData = [
+      { name: "아메리카노", stock: 0, price: 3000 },
+      { name: "우유라떼", stock: 0, price: 3500 },
+      { name: "두유라떼", stock: 0, price: 4000 },
+    ];
+    data.saveData("coffeeMenu", initData);
+    return data.getData();
   }
 };
 
--- Cafe/manager/data.js
+++ Cafe/manager/data.js
@@ -5,18 +5,22 @@ class DataManager {
   makeObj(data) {
     return { name: data.getName(), stock: data.getStock(), price: data.getPrice() };
   }
-
   format(key, value) {
     return { [key]: value };
   }
 
+  existData() {
+    return fs.existsSync(env.dataStore);
+  }
   saveData(keyName, data) {
     const formatted = this.format(
       keyName,
       data.map((v) => this.makeObj(v))
     );
-    console.log(formatted);
     fs.writeFileSync(env.dataStore, JSON.stringify(formatted), "utf-8");
+  }
+  getData() {
+    return JSON.parse(fs.readFileSync(env.dataStore, "utf-8"));
   }
 }
 module.exports = { DataManager };
